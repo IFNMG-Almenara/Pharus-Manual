00f84fb930edb6907df828dfb6738167f2c3b69e
lipejar <71912649+lipejar@users.noreply.github.com>
2023-04-07 11:39:32 -0300

    Alteração no footer
    Deixei o footer similar ao footer da página do pharus

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -30,33 +30,25 @@ class Footer extends React.Component {
             )}
           </a>
           <div>
-            <h5>Docs</h5>
-            <a href={this.docUrl('doc1.html')}>
-              Getting Started (or other categories)
+            {/* <h5>Docs</h5> */}
+            <a href="https://eventos.ifnmg.edu.br/#eventos">
+              Outros Eventos
             </a>
-            <a href={this.docUrl('doc2.html')}>Guides (or other categories)</a>
-            <a href={this.docUrl('doc3.html')}>
-              API Reference (or other categories)
-            </a>
-          </div>
-          <div>
-            <h5>Community</h5>
-            <a href={`${this.props.config.baseUrl}users`}>User Showcase</a>
-            <a
-              href="https://stackoverflow.com/questions/tagged/"
-              target="_blank"
-              rel="noreferrer noopener">
-              Stack Overflow
-            </a>
-            <a href="https://discordapp.com/">Project Chat</a>
-            <a
-              href="https://twitter.com/"
-              target="_blank"
-              rel="noreferrer noopener">
-              Twitter
+            <a href="https://eventos.ifnmg.edu.br/#certificados">
+              Validação de Certificados
+              </a>
+            <a href="https://eventos.ifnmg.edu.br/area-do-participante/meus-eventos">
+              Área do Participante
             </a>
           </div>
           <div>
+            <a>Desenvolvido pelo IFNMG - Campus Almenara</a>
+            <a
+              href="mailto:pharus@ifnmg.edu.br">
+              pharus@ifnmg.edu.br
+            </a>
+          </div>
+          {/* <div>
             <h5>More</h5>
             <a href={`${this.props.config.baseUrl}blog`}>Blog</a>
             <a href="https://github.com/">GitHub</a>
@@ -92,7 +84,7 @@ class Footer extends React.Component {
                 />
               </div>
             )}
-          </div>
+          </div> */}
         </section>
 
         {/* <a
